2f2c8446804de702383bffc7f984117032046138
Grishberg <grishberg@ya.ru>
2015-07-28 08:23:00 +0700

    add project's files

--- app/src/main/java/info/goodline/btv/ui/adapters/RvAdapter.java
+++ app/src/main/java/info/goodline/btv/ui/adapters/RvAdapter.java
@@ -2,20 +2,25 @@ package info.goodline.btv.ui.adapters;
 
 import android.content.Context;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import info.goodline.btv.android_btvc.R;
+import info.goodline.btv.ui.view.RatingDraggablePanel;
 import info.goodline.btv.ui.view.SwipePanel;
 
 /**
  * Created by g on 26.07.15.
  */
 public class RvAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+    public static final String TAG = RvAdapter.class.getSimpleName();
     private String[] mDataset;
     private LayoutInflater mInflater;
     private Context mContext;
+    private static final int TYPE_RATE = 1;
+    private static final int TYPE_SWIPE = 2;
 
     public RvAdapter(Context context, String[] dataSet) {
         mDataset = dataSet;
@@ -25,7 +30,24 @@ public class RvAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
     }
 
     @Override
+    public int getItemViewType(int position) {
+        if (position == 0) return TYPE_RATE;
+        return TYPE_SWIPE;
+    }
+
+    @Override
     public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup viewGroup, int i) {
+        Log.d(TAG, "onCreateView i=" + i);
+        switch (i) {
+            case TYPE_RATE:
+                return getRateHolder(viewGroup);
+            case TYPE_SWIPE:
+                return getSwipeHolder(viewGroup);
+        }
+        return null;
+    }
+
+    private RecyclerView.ViewHolder getRateHolder(ViewGroup viewGroup) {
         SwipePanel swipePanel = new SwipePanel(viewGroup.getContext());
         swipePanel.setLayoutParams(
                 new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,
@@ -47,9 +69,35 @@ public class RvAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
         return viewHolder;
     }
 
+    private RecyclerView.ViewHolder getSwipeHolder(ViewGroup viewGroup) {
+        View infoView = mInflater.inflate(R.layout.view_info_container, null);
+        infoView.setLayoutParams(
+                new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,
+                        ViewGroup.LayoutParams.MATCH_PARENT)
+        );
+        View draggView = infoView.findViewById(R.id.llDragContainer);
+        RatingDraggablePanel ratingDraggablePanel = new RatingDraggablePanel(viewGroup.getContext());
+        ratingDraggablePanel.setLayoutParams(
+                new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,
+                        ViewGroup.LayoutParams.WRAP_CONTENT)
+        );
+        ratingDraggablePanel.init(infoView, draggView);
+        return new ViewHolderRate(ratingDraggablePanel);
+    }
+
     @Override
-    public void onBindViewHolder(RecyclerView.ViewHolder viewHolder, int i) {
-        ViewHolder holder = (ViewHolder) viewHolder;
+    public void onBindViewHolder(RecyclerView.ViewHolder viewHolder, int position) {
+        switch (getItemViewType(position)) {
+            case TYPE_RATE:
+                Log.d(TAG, "rate pos=" + position + " type=" + getItemViewType(position));
+                //ViewHolderRate holderRate = (ViewHolderRate) viewHolder;
+                break;
+
+            case TYPE_SWIPE:
+                Log.d(TAG, "swipe pos=" + position + " type=" + getItemViewType(position));
+                //ViewHolder holder = (ViewHolder) viewHolder;
+                break;
+        }
     }
 
     @Override
@@ -63,6 +111,15 @@ public class RvAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
         public ViewHolder(SwipePanel holderView) {
             super(holderView);
             swipePanel = holderView;
+        }
+    }
+
+    public static class ViewHolderRate extends RecyclerView.ViewHolder {
+        RatingDraggablePanel ratingDraggablePanel;
+
+        public ViewHolderRate(RatingDraggablePanel holderView) {
+            super(holderView);
+            ratingDraggablePanel = holderView;
         }
     }
 }
--- app/src/main/java/info/goodline/btv/ui/view/RatingDraggablePanel.java
+++ app/src/main/java/info/goodline/btv/ui/view/RatingDraggablePanel.java
@@ -1,29 +1,43 @@
 package info.goodline.btv.ui.view;
 
 import android.content.Context;
+import android.graphics.Rect;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 
 import info.goodline.btv.android_btvc.R;
+import info.goodline.btv.ui.ICanScrollInterface;
 
 /**
  * Created by g on 23.07.15.
  */
-public class RatingDraggablePanel extends LinearLayout implements View.OnTouchListener {
+public class RatingDraggablePanel extends LinearLayout implements View.OnTouchListener ,
+        ICanScrollInterface {
     private static final String TAG = SwipePanel.class.getSimpleName();
+    private static final float SCROLL = 0.15f;
+
     private int mRightPanelWidth;
     private int mLeftPanelWidth;
+    private int mDraggWidth;
+    private int mDraggContainerWidth;
+    private int mDraggLeftOffset;
     LinearLayout llLeftPanel;
     LinearLayout llRightPanel;
+    private View mMainView;
+    private View mDraggView;
     private int mLastX;
     private float m;
+    private boolean mIsHidded;
 
     public RatingDraggablePanel(Context context) {
         super(context);
+        inflate(getContext(), R.layout.view_rating_draggable_panel, this);
+        init();
     }
 
     public RatingDraggablePanel(Context context, AttributeSet attrs) {
@@ -32,19 +46,61 @@ public class RatingDraggablePanel extends LinearLayout implements View.OnTouchLi
         init();
     }
 
+    public void init(View mainPanel, View dragContainer){
+        mMainView = mainPanel;
+        mDraggView = dragContainer;
+        llRightPanel.addView(mMainView);
+        mDraggView.setOnTouchListener(this);
+    }
+
     private void init() {
         llLeftPanel = (LinearLayout) findViewById(R.id.llLeft);
         llRightPanel = (LinearLayout) findViewById(R.id.llMain);
-        llRightPanel.setOnTouchListener(this);
-        addOnLayoutChangeListener(new OnLayoutChangeListener() {
-            @Override
-            public void onLayoutChange(View v, int left, int top, int right, int bottom, int oldLeft, int oldTop, int oldRight, int oldBottom) {
-                if (mRightPanelWidth == 0) {
-                    mRightPanelWidth = right - left;
-                    changeWidth(mRightPanelWidth);
-                }
-            }
-        });
+        llLeftPanel.setVisibility(GONE);
+        mIsHidded = true;
+//        llRightPanel.setOnTouchListener(this);
+    }
+
+
+    @Override
+    public boolean isCanScroll(int x) {
+        int width = getWidth();
+        int slideWidth = Math.round(width * SCROLL);
+        if (x < slideWidth || x > width - slideWidth) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        if(mRightPanelWidth == 0 ){
+            mRightPanelWidth = getMeasuredWidth();
+        }
+
+        if(mDraggWidth == 0) {
+            mLeftPanelWidth = llLeftPanel.getMeasuredWidth();
+            mDraggView.measure(MeasureSpec.UNSPECIFIED, MeasureSpec.UNSPECIFIED);
+            mDraggContainerWidth = mDraggView.getMeasuredWidth();
+            mDraggWidth = mRightPanelWidth - mDraggContainerWidth;
+            changeLeftPanelWidth(mDraggWidth);
+            //TODO: get left offset
+            Rect outRect = new Rect();
+            int[] location = new int[2];
+
+            mDraggView.getDrawingRect(outRect);
+            mDraggView.getLocationOnScreen(location);
+            outRect.offset(location[0], location[1]);
+            mDraggLeftOffset = outRect.left;
+        }
+        setMeasuredDimension(mRightPanelWidth + mDraggWidth, getMeasuredHeight());
+    }
+
+    private void changeLeftPanelWidth(int width){
+        ViewGroup.LayoutParams params = llLeftPanel.getLayoutParams();
+        params.width = width;
+        llLeftPanel.setLayoutParams(params);
     }
 
     private void changeWidth(int width) {
@@ -62,8 +118,20 @@ public class RatingDraggablePanel extends LinearLayout implements View.OnTouchLi
         setLayoutParams(params);
     }
 
+    private void changeLeftMargin(int offset) {
+        if (getLayoutParams() instanceof ViewGroup.MarginLayoutParams) {
+            ViewGroup.MarginLayoutParams p = (ViewGroup.MarginLayoutParams) getLayoutParams();
+            p.setMargins(offset, p.topMargin, p.rightMargin, p.bottomMargin);
+            requestLayout();
+        }
+    }
     @Override
     public boolean onTouch(View v, MotionEvent event) {
+        if(mIsHidded){
+            mIsHidded = false;
+            llLeftPanel.setVisibility(VISIBLE);
+            changeLeftMargin( -mDraggWidth);
+        }
         final int x = (int) event.getRawX();
         final int y = (int) event.getRawY();
         switch (event.getAction()) {
